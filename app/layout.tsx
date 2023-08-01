5c3a02d6afaa80bfcf1cae92fd4f8d31da283cff
Pei <pswu@protonmail.com>
2023-08-01 16:58:07 +0200

    add cn zod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Providers from './provider';
+import Providers from './utils/provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
