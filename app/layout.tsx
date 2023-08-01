a66729996654bb2c446e83bec4aab679f9fb3944
Pei <pswu@protonmail.com>
2023-08-01 15:54:15 +0200

    setup react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'there will be a name',
   description: 'A job search organizer...',
-}
+};
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </QueryClientProvider>
+  );
 }
