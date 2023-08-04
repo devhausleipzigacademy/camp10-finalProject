c023d1fde02ad6ae251950a678af6cf64b33f220
hottinho <christian.erll@gmail.com>
2023-08-04 10:58:53 +0200

    rearranged layout grouping

--- app/(dashboard)/profile/[profile]/page.tsx
+++ app/(dashboard)/profile/[profile]/page.tsx
@@ -1,3 +1,3 @@
 export default function Profile({ params }: { params: { profile: number } }) {
-    return <div>Whos Profile is this: {params.profile}</div>
+  return <div>Whose Profile is this: {params.profile}</div>;
 }
