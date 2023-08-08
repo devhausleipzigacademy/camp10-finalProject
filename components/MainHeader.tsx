21ee2756ad16d12bf0ec7681ee0e9e6e14d32cec
Pei <pswu@protonmail.com>
2023-08-08 16:53:01 +0200

    conditional navlink

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -2,33 +2,36 @@ import React from 'react';
 import Link from 'next/link';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
-// type HeaderProps = {
-//   underline: boolean;
-// };
-
 export function MainHeader() {
+  // TODO: replace with clerk auth
+  // TODO: replace with Button component
+  const isAuth = false;
   return (
-    <header
-      className={`flex justify-between w-full 2xl:max-w-7xl py-m`}
-    >
+    <header className={`flex justify-between w-full 2xl:max-w-7xl py-m`}>
       <Link href="/" className="font-500 text-xxl">
         EMPLEO.
       </Link>
       <nav className="space-x-s flex items-center">
         <HiArrowNarrowRight className="text-xl" />
-        <Link href="/login">
+        {!isAuth && (
+          <>
+            <Link href="/login">
+              <button className="border border-basicColors-light rounded-full px-s py-xxs hover:cursor-pointer">
+                Login
+              </button>
+            </Link>
+            <Link href="/registration">
+              <button className="border border-basicColors-light rounded-full px-s py-xxs hover:cursor-pointer">
+                Register
+              </button>
+            </Link>
+          </>
+        )}
+        {isAuth && (
           <button className="border border-basicColors-light rounded-full px-s py-xxs hover:cursor-pointer">
-            Login
+            Logout
           </button>
-        </Link>
-        <Link href="/registration">
-          <button className="border border-basicColors-light rounded-full px-s py-xxs hover:cursor-pointer">
-            Register
-          </button>
-        </Link>
-        <button className="border border-basicColors-light rounded-full px-s py-xxs hover:cursor-pointer">
-          Logout
-        </button>
+        )}
       </nav>
     </header>
   );
