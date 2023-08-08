f8ad3e2fdaf526d880db312c3a750421b411ac62
Pei <pswu@protonmail.com>
2023-08-08 16:20:47 +0200

    header text

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,11 +8,11 @@ export default function AuthLayout({
   return (
     <main className="h-full flex flex-col">
       <div className='border-x-0 border-b border-basicColors-light'></div>
-      <h1 className="text-headerOne flex uppercase text-justify leading-[1] font-800 py-s">
+      <h1 className="sm:text-headerOne text-xxxl flex uppercase text-justify leading-[1] font-800 py-s">
         Streamline your job hunt with us.
       </h1>
       {/* TODO: style the auth ui after clerk is set up */}
-      <div className="w-[560px] h-[360px] border border-basicColors-light -mt-xl relative m-auto ui-background rounded-xl">
+      <div className="w-[560px] h-[360px] border border-basicColors-light md:-mt-xl relative m-auto ui-background rounded-xl">
         {children}
       </div>
     </main>
