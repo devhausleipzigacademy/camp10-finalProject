3519175ed4331b4eaa5080dea07497649ae18ba8
JanHendryk <j.h.muenchow@posteo.de>
2023-08-11 12:10:55 +0200

    clerk, signIn, signOut, little css

--- app/(auth)/registration/[[...registration]]/page.tsx
+++ app/(auth)/registration/[[...registration]]/page.tsx
@@ -4,11 +4,13 @@ import { dark } from '@clerk/themes';
 export default function RegistrationForm() {
     return (
         <div
-        // className="top-l w-[560px] h-[360px] border border-basicColors-light m-auto ui-background rounded-xl mt-l"
+            className="h-[100%] w-[100%]  flex flex-row justify-center"
+            // className="top-l w-[560px] h-[360px] border border-basicColors-light m-auto ui-background rounded-xl mt-l"
         >
             <SignUp
                 appearance={{
                     baseTheme: dark,
+                    
                 }}
             />
         </div>
