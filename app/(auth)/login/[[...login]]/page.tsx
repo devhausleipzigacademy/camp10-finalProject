c52b58deb233318380a0ebb05414fd2b1a919c6e
JanHendryk <j.h.muenchow@posteo.de>
2023-08-11 10:17:34 +0200

    clerk implemented, using dark theme

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,28 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
-
 export default function LoginForm() {
-  return (
-    //  <div className="top-l w-[560px] h-[360px] border border-basicColors-light m-auto ui-background rounded-xl mt-l">
-    //      Login Form
-    //  </div>
-    <SignIn
-      appearance={{
-        elements: {
-          card: 'bg-basicColors-dark',
-          headerTitle: '',
-          headerSubtitle: '',
-          main: '',
-          socialButtons: '',
-          socialButtonsBlockButton: '',
-          //How should we decide between git/ gmail
-          dividerRow: '',
-          dividerText: '',
-          formFieldLabelRow: '',
-
-          formButtonPrimary: 'bg-[#e74c3c]',
-        },
-      }}
-    />
-  );
+    return (
+        <SignIn
+            appearance={{
+                baseTheme: dark,
+            }}
+        />
+    );
 }
--- app/(auth)/registration/[[...registration]]/page.tsx
+++ app/(auth)/registration/[[...registration]]/page.tsx
@@ -1,7 +1,16 @@
+import { SignUp } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
+
 export default function RegistrationForm() {
-  return (
-    <div className="top-l w-[560px] h-[360px] border border-basicColors-light m-auto ui-background rounded-xl mt-l">
-      Sign up Form
-    </div>
-  );
+    return (
+        <div
+        // className="top-l w-[560px] h-[360px] border border-basicColors-light m-auto ui-background rounded-xl mt-l"
+        >
+            <SignUp
+                appearance={{
+                    baseTheme: dark,
+                }}
+            />
+        </div>
+    );
 }
