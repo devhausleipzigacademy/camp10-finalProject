cc677071bac4bc692fcdfbe093a94c05ac40738c
Pei <pswu@protonmail.com>
2023-08-14 17:26:14 +0200

    add sample prisma code for job

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,22 +1,9 @@
 // import BasicTable from '@/components/BasicTable';
 // import Board from '@/components/Board';
 import dynamic from 'next/dynamic';
-// import { PrismaClient } from '@prisma/client';
 
 const BoardNoSSR = dynamic(() => import('@/components/Board'), { ssr: false });
-// const getData = async () => {
-//     // select title of column from job table based on the relation
-//     const prisma = new PrismaClient()
-//     prisma.job.findMany({
-//         select: {
-//             column: {
-//                 select: {
-//                     title: true
-//                 }
-//             }
-//         }
-//     })
-// };
+
 export default function KanbanBoard() {
     return <BoardNoSSR />;
     // return <BasicTable />;
