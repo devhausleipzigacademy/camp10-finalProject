205daae7746f56b2d8e179050bd0a541d6c91dfb
Pei <pswu@protonmail.com>
2023-08-14 11:51:46 +0200

    merge main

--- components/shared/Input.tsx
+++ components/shared/Input.tsx
@@ -28,4 +28,35 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ label, isRequire
   );
 })
 
+
+// interface Props extends Input {
+//   id: string;
+//   label: string;
+//   isRequired: boolean;
+// }
+
+// const Input = React.forwardRef<HTMLInputElement, Props>(
+//   ({ id, label, ...props }, ref) => {
+//     return (
+//       <div>
+//         <label
+//           htmlFor={id}
+//           className="text-xs mx-6 text-neutral-400 font-semibold capitalize"
+//         >
+//           {label}
+//         </label>
+//         <input
+//           ref={ref}
+//           id={id}
+//           className="border-solid border-gray border-2 px-6 py-2 text-sm text-neutral-500 rounded-md w-full focus:border-primary focus:outline-none"
+//           {...props}
+//         />
+//         {/* {error && (
+//           <small className="text-red-500 text-xs">{error.message}</small>
+//         )} */}
+//       </div>
+//     );
+//   }
+// );
+
 export default Input;
