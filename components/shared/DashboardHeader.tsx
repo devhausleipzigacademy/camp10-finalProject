cc7e0d2035f9fc5e3738ce78b508d2077eff7f0f
JanHendryk <j.h.muenchow@posteo.de>
2023-08-17 12:32:10 +0200

    Toogle Button implemented, css styling adjusted

--- components/shared/DashboardHeader.tsx
+++ components/shared/DashboardHeader.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import SearchInput from './SearchInput';
 import dynamic from 'next/dynamic';
 import BasicTable from '../BasicTable';
-import { HiOutlineViewGrid } from 'react-icons/hi';
 import { HiOutlineListBullet } from 'react-icons/hi2';
 import { SlGrid } from 'react-icons/sl';
 import { cn } from '@/utils/cn';
@@ -20,25 +19,38 @@ export default function DashboardHeader() {
     const [toggleViewMode, setToggleViewMode] = useState(false);
     return (
         <div className="ml-xl">
-            <header className="bg-basicColors-dark w-full h-[80px] flex flex-row justify-between py-s pl-m ">
+            <header className="bg-basicColors-dark container h-[80px] flex flex-row justify-between py-s pl-m ">
                 <SearchInput onSearch={handleSearch} />
 
                 <div
                     onClick={() => setToggleViewMode(!toggleViewMode)}
-                    className=" flex items-center justify-center gap-s px-s border-2 rounded-xl border-basicColors-light mr-m"
+                    className=" flex items-center justify-center  border-2 rounded-xl border-basicColors-light mr-m"
                 >
                     <div
                         className={cn(
-                            'w-[44px] h-[44px]',
+                            'w-[68.5px] h-[49px] rounded-l-xl px-m py-[0.725rem]',
                             toggleViewMode
-                                ? 'bg-basicColors-light'
-                                : 'bg-basicColors-dark'
+                                ? 'bg-basicColors-dark'
+                                : 'bg-basicColors-light'
                         )}
                     >
-                        <SlGrid />
+                        <SlGrid
+                            size={25}
+                            color={!toggleViewMode ? '#3D3D3D' : '#F5F7FE'}
+                        />
                     </div>
-                    <div className="bg-basicColors-dark">
-                        <HiOutlineListBullet />
+                    <div
+                        className={cn(
+                            'w-[68.5px] h-[49px] rounded-r-xl px-m py-[0.725rem]',
+                            !toggleViewMode
+                                ? 'bg-basicColors-dark'
+                                : 'bg-basicColors-light'
+                        )}
+                    >
+                        <HiOutlineListBullet
+                            size={25}
+                            color={toggleViewMode ? '#3D3D3D' : '#F5F7FE'}
+                        />
                     </div>
                 </div>
             </header>
