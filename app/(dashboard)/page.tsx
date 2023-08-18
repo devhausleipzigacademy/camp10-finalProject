2d81fae0df1b6585828dfac933c697e8bf350142
Pei <pswu@protonmail.com>
2023-08-18 09:26:59 +0200

    cleanup

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import { auth } from '@clerk/nextjs';
-import { Column, Job } from '@prisma/client';
 import { getColumns } from './getColumns';
 
 
@@ -9,8 +8,7 @@ const BoardNoSSR = dynamic(() => import('@/components/Board'), { ssr: false });
 export default async function Dashboard() {
     const { userId } = auth();
     let userColumns = await getColumns(userId as string);
-    // console.log('userColumnsFromDB:', userColumns);
-
+    
     return <BoardNoSSR columnData={userColumns} />;
 }
 
