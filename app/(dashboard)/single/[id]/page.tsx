de995cb12b850353f0cfe023e22deffa514915b8
JanHendryk <j.h.muenchow@posteo.de>
2023-08-28 14:01:43 +0200

    prisma

--- app/(dashboard)/single/[id]/page.tsx
+++ app/(dashboard)/single/[id]/page.tsx
@@ -1,27 +1,20 @@
 import prisma from '@/utils/prismaClient';
 
- async function getData(jobId: string) {
-    
-  const singleJob = await prisma.job.findFirst({
-
-
-        
-            where: {
-
-                id: jobId
-            }
-            
-
-            
-        
+async function getData(jobId: string) {
+    const singleJob = await prisma.job.findFirst({
+        where: {
+            id: jobId,
+        },
     });
-    return singleJob
+    return singleJob;
 }
 
-
-export default async function SingleJob({ params }: { params: { id: string } }) {
-     const singleJob =  await getData(params.id);
+export default async function SingleJob({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const singleJob = await getData(params.id);
 
     return <div>My Job: {singleJob?.title}</div>;
 }
-
