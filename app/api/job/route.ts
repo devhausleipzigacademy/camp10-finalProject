3dab04a58e9d2a62c404d910e19d0edddddf30dd
JanHendryk <j.h.muenchow@posteo.de>
2023-09-01 09:44:16 +0200

    fixed bug

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -11,6 +11,7 @@ export const POST = authHandler(async ({ userId, body }) => {
         const newJob = await prisma.job.create({
             data: {
                 ...body,
+                deadline: new Date (body.deadline),
                 userId,
             },
         });
